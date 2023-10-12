ae413fbc0539669429c5fa567871c57113b13e17
Sai Sudheer Vishnumolakala <57912029+sudheer490@users.noreply.github.com>
2023-10-12 11:36:35 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+from sklearn.externals import joblib  # For loading the trained model
+from your_confusion_matrix_function import make_confusion_matrix  # Import your custom confusion matrix function here
+
+# Load the trained model and preprocessing functions
+model = joblib.load('your_trained_model.pkl')  # Replace 'your_trained_model.pkl' with the actual filename of your trained model
+# Load other preprocessing functions if you have any
+
+# Function to preprocess input data
+def preprocess_data(data):
+    # Apply the same feature encoding as done during training
+    data['ADDRTYPE'].replace(to_replace=['Intersection', 'Block', 'Alley', 'Unknown'], value=[1, 2, 3, 0], inplace=True)
+    # Apply similar encoding for other features
+    # ...
+
+    # Return the preprocessed data
+    return data
+
+# Streamlit UI elements
+st.title('Accident Severity Prediction')
+st.write('Enter the details below:')
+
+# User input fields
+addrtype = st.selectbox('Address Type', ['Intersection', 'Block', 'Alley', 'Unknown'])
+# Add more input fields for other features
+
+# Preprocess user input
+user_data = {'ADDRTYPE': [addrtype]}  # Initialize with user input
+df = pd.DataFrame(user_data)
+preprocessed_df = preprocess_data(df)
+
+# Make predictions using the loaded model
+prediction = model.predict(preprocessed_df)
+
+# Display prediction
+st.write('Predicted Severity:', prediction)
+
+# Optionally, you can display other information or visualizations related to the prediction
